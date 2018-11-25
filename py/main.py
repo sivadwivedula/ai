300ad2c6659cfffc0edefe7d0c827c96232bce2a
sivadwivedula <shivadwivedula3@gmail.com>
2018-11-25 11:00:05 -0800

    testfile

--- py/main.py
+++ py/main.py
@@ -1,10 +1,19 @@
 ''' this is the program tochekck if my machine is understanding the user kkd
 sd'''
-def readf(a,b='r'):
+def readf(fname,permission='r'):
     try:
         
-        with open("C:/Users/Administrator/Desktop/files/"+a+".txt",b) as f:
+        with open("C:/Users/Administrator/Desktop/ever/files/"+fname+".txt",permission) as f:
             a=(f.read().split("\n"))
+    except:
+        return "specify the name properly"
+    else:
+        return a
+def writef(data="",fname="basic",permission='a+'):
+    try:
+        
+        with open("C:/Users/Administrator/Desktop/ever/db/"+fname+".txt",permission) as f:
+            a=f.write(data+"\n")
     except:
         return "specify the name properly"
     else:
